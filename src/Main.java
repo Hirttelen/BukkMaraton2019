e6bfaa9f7adc78963af717ddf2b510f9dc277016
Hirttelen <hirt.gergo@gmail.com>
2022-12-19 21:50:42 +0100

    Létrehozás után első pár feladat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            List<versenyzo> emberek = new ArrayList<>();
+            BufferedReader reader = new BufferedReader(new FileReader("bukkm2019.txt"));
+            reader.readLine();
+            String Sor;
+            while ((Sor = reader.readLine()) != null) {
+                String[] sor = Sor.split(";");
+                versenyzo versenyzo = new versenyzo();
+                versenyzo.rajtszam = sor[0];
+                versenyzo.kategoria = sor[1];
+                versenyzo.nev = sor[2];
+                versenyzo.egyesulet = sor[3];
+                versenyzo.ido = sor[4];
+                emberek.add(versenyzo);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();}
     }
 }
---
+++ src/versenyzo.java
@@ -0,0 +1,7 @@
+public class versenyzo {
+        public String rajtszam;
+        public String kategoria;
+        public String nev;
+        public String egyesulet;
+        public String ido;
+    }
